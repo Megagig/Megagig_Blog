426839820ece32d565bcc97627375180b9004271
Megagig <megagigsoftwaresolutions.com>
2025-01-17 17:53:30 +0100

    Create async error and Apperror functions to handle async errors and application errors respectively

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -9,6 +9,7 @@ export const signup = async (req, res) => {
     res.status(500).json({ error: error.message });
   }
 };
+
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
