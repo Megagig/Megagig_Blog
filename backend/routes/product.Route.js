8862d0c52f9259693ee590b97b3b8498dd54e961
Megagig <megagigsoftwaresolutions.com>
2025-01-26 16:54:15 +0100

    Add the Products routes

--- backend/routes/product.Route.js
+++ backend/routes/product.Route.js
@@ -2,12 +2,18 @@ import express from 'express';
 import {
   createProduct,
   getAllProducts,
+  getSingleProduct,
+  updateProduct,
+  deleteProduct,
 } from '../controllers/productController.js';
+import { authenticateUser } from '../middleware/authenticateUser.js';
 
 const router = express.Router();
 
-router.post('/', createProduct);
-
+router.post('/', authenticateUser, createProduct);
 router.get('/', getAllProducts);
+router.get('/:id', getSingleProduct);
+router.patch('/:id', authenticateUser, updateProduct);
+router.delete('/:id', authenticateUser, deleteProduct);
 
 export default router;
