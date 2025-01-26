40d5815e0ddbcdd645246ed94702c4c3dcc60484
Megagig <megagigsoftwaresolutions.com>
2025-01-26 10:55:27 +0100

    Add Post, get all comments, get total comments and delete a comment routes

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -14,3 +14,26 @@ export const createComment = catchAsync(async (req, res, next) => {
     .status(201)
     .send({ message: 'Comment posted successfully', comment: newComment });
 });
+
+// Get all comments (public route)
+export const getAllComments = catchAsync(async (req, res, next) => {
+  const comments = await Comment.find().populate('user', 'username email');
+  res.status(200).json({ comments });
+});
+
+// Get total comments (private route for admin dashboard)
+export const getTotalComments = catchAsync(async (req, res, next) => {
+  const totalComments = await Comment.countDocuments({});
+  res.status(200).send({ totalComments });
+});
+
+// Delete comment (protected route for admin dashboard)
+export const deleteComment = catchAsync(async (req, res, next) => {
+  const { commentId } = req.params;
+  const comment = await Comment.findById(commentId);
+  if (!comment) {
+    return next(new AppError('Comment not found', 404));
+  }
+  await Comment.findByIdAndDelete(commentId);
+  res.status(200).send({ message: 'Comment deleted successfully' });
+});
--- backend/routes/comment.Route.js
+++ backend/routes/comment.Route.js
@@ -1,8 +1,17 @@
 import express from 'express';
 
-import { createComment } from '../controllers/commentController.js';
+import {
+  createComment,
+  deleteComment,
+  getAllComments,
+  getTotalComments,
+} from '../controllers/commentController.js';
 
 const router = express.Router();
 
 router.post('/', createComment);
+router.get('/', getAllComments);
+router.get('/total-comments', getTotalComments);
+router.delete('/:commentId', deleteComment);
+
 export default router;
